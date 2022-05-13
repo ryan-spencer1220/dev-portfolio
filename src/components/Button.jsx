a9dc0935274d264ab1742fec2979e6cbdd5d7ed4
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-05-13 09:10:36 -0700

    Add DaisyUI and technology cards

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,14 +2,7 @@ import React from "react";
 import "../index.css";
 
 const Button = (props) => {
-  return (
-    <button
-      className="bg-green-800 text-white font-[Poppins] py-2 px-6 rounded md:ml-8 hover:bg-green-600
-    duration-500"
-    >
-      {props.children}
-    </button>
-  );
+  return <button className="btn btn-lg">{props.children}</button>;
 };
 
 export default Button;
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -6,9 +6,40 @@ const Technologies = () => {
       <div className="flex flex-wrap md items-center h-screen pt-18">
         <div className=" bg-slate-100 w-full h-screen">
           <div className="mx-32">
-            <h1 className="text-6xl font-bold mt-40 pt-18 text-center">
+            <h1 className="text-6xl font-bold m-20 pt-18 text-center">
               Technologies
             </h1>
+            <div className="flex flex-col w-full lg:flex-row mt-20">
+              <div className="flex flex-col w-full lg:flex-row">
+                <div className="grid flex-grow h-96 w-96 card bg-base-300 rounded-box place-items-center">
+                  React
+                </div>
+                <div className="divider lg:divider-horizontal"></div>
+                <div className="grid flex-grow h-96 w-96 card bg-base-300 rounded-box place-items-center">
+                  C#
+                </div>
+                <div className="divider lg:divider-horizontal"></div>
+                <div className="grid flex-grow h-96 w-96 card bg-base-300 rounded-box place-items-center">
+                  Node.js
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col w-full lg:flex-row mt-20">
+              <div className="flex flex-col w-full lg:flex-row">
+                <div className="grid flex-grow h-96 w-96 card bg-base-300 rounded-box place-items-center">
+                  JavaScript
+                </div>
+                <div className="divider lg:divider-horizontal"></div>
+                <div className="grid flex-grow h-96 w-96 card bg-base-300 rounded-box place-items-center">
+                  MySQL
+                </div>
+                <div className="divider lg:divider-horizontal"></div>
+                <div className="grid flex-grow h-96 w-96 card bg-base-300 rounded-box place-items-center">
+                  MongoDB
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
