d78adca2504bc677269d01918a8d9b9e5037a1ca
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-05-18 11:09:23 -0700

    WIP: Animate stats

--- src/API/ChessResults.jsx
+++ src/API/ChessResults.jsx
@@ -21,18 +21,21 @@ function ChessResults() {
       <div className="stat place-items-center">
         <div className="stat-title">Rapid Elo</div>
         {/* <div className="stat-value">{users.chess_rapid.best.rating}</div> */}
+        <div className="stat-value">1659</div>
         <div className="stat-desc">10 - 30 Min Rating</div>
       </div>
 
       <div className="stat place-items-center">
         <div className="stat-title">Blitz Elo</div>
         {/* <div className="stat-value">{users.chess_blitz.best.rating}</div> */}
+        <div className="stat-value">1508</div>
         <div className="stat-desc">3 - 5 Min Rating</div>
       </div>
 
       <div className="stat place-items-center">
         <div className="stat-title">Tactics Elo</div>
         {/* <div className="stat-value">{users.tactics.highest.rating}</div> */}
+        <div className="stat-value">2586</div>
         <div className="stat-desc">Puzzle Rating</div>
       </div>
 
@@ -43,6 +46,7 @@ function ChessResults() {
             users.chess_blitz.record.win +
             users.chess_blitz.record.loss}
         </div> */}
+        <div className="stat-value text-secondary">7454</div>
         <div className="stat-desc">Since 2014</div>
       </div>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,23 +1,9 @@
-import React from "react";
 import barChart from "../assets/projects/bar-chart.png";
 import weather from "../assets/projects/rain.png";
 import coding from "../assets/projects/web-development.png";
 import github from "../assets/projects/github-sign.png";
 import beer from "../assets/projects/beer-mug.png";
 import quiz from "../assets/projects/quiz.png";
-import { IconContext } from "react-icons";
-import { GoMarkGithub } from "react-icons/go";
-import { GrLinkedin } from "react-icons/gr";
-import { BsMedium } from "react-icons/bs";
-import githubFinder from "../assets/screenshots/github-finder.png";
-import epicodusTaproom from "../assets/screenshots/epicodus-taproom.png";
-import languageQuiz from "../assets/screenshots/language-quiz.png";
-import sortingVisualizer from "../assets/screenshots/sorting-visualizer.png";
-import weatherAPI from "../assets/screenshots/weather-api.png";
-import { SiJavascript } from "react-icons/si";
-import { DiReact } from "react-icons/di";
-import { SiTailwindcss } from "react-icons/si";
-import { SiFirebase } from "react-icons/si";
 import GitHubFinder from "../modals/GithubFinder";
 import DevPortfolio from "../modals/DevPortfolio";
 import LanguageQuiz from "../modals/LanguageQuiz";
@@ -87,7 +73,7 @@ const Projects = () => {
 
                 <div className="divider lg:divider-horizontal"></div>
 
-                <div className="grid flex-grow h-72 w-72   card bg-base-300 rounded-box place-items-center shadow-2xl">
+                <div className="grid flex-grow h-72 w-72 card bg-base-300 rounded-box place-items-center shadow-2xl">
                   <figure>
                     <img
                       src={coding}
@@ -110,23 +96,7 @@ const Projects = () => {
                     id="my-modal-9"
                     className="modal-toggle"
                   />
-                  <div className="modal modal-bottom sm:modal-middle">
-                    <div className="modal-box relative">
-                      <label
-                        htmlFor="my-modal-9"
-                        className="btn btn-sm btn-circle absolute right-2 top-2"
-                      >
-                        ✕
-                      </label>
-                      <h3 className="text-lg font-bold text-accent">
-                        Congratulations random Interner use
-                      </h3>
-                      <p className="py-4">
-                        You've been selected for a chance to get one year of
-                        subscription to use Wikipedia for free!
-                      </p>
-                    </div>
-                  </div>
+                  <DevPortfolio />
                 </div>
               </div>
             </div>
@@ -156,28 +126,12 @@ const Projects = () => {
                     id="my-modal-10"
                     className="modal-toggle"
                   />
-                  <div className="modal modal-bottom sm:modal-middle">
-                    <div className="modal-box relative">
-                      <label
-                        htmlFor="my-modal-10"
-                        className="btn btn-sm btn-circle absolute right-2 top-2"
-                      >
-                        ✕
-                      </label>
-                      <h3 className="text-lg font-bold text-accent">
-                        Goal Tracker Application
-                      </h3>
-                      <p className="py-4">
-                        You've been selected for a chance to get one year of
-                        subscription to use Wikipedia for free!
-                      </p>
-                    </div>
-                  </div>
+                  <WeatherAPI />
                 </div>
 
                 <div className="divider lg:divider-horizontal"></div>
 
-                <div className="grid flex-grow h-72 w-72   card bg-base-300 rounded-box place-items-center shadow-2xl">
+                <div className="grid flex-grow h-72 w-72 card bg-base-300 rounded-box place-items-center shadow-2xl">
                   <figure>
                     <img src={beer} alt="Beer Mug" height={120} width={120} />
                   </figure>
@@ -195,28 +149,12 @@ const Projects = () => {
                     id="my-modal-11"
                     className="modal-toggle"
                   />
-                  <div className="modal modal-bottom sm:modal-middle">
-                    <div className="modal-box relative">
-                      <label
-                        htmlFor="my-modal-11"
-                        className="btn btn-sm btn-circle absolute right-2 top-2"
-                      >
-                        ✕
-                      </label>
-                      <h3 className="text-lg font-bold text-accent">
-                        Goal Tracker Application
-                      </h3>
-                      <p className="py-4">
-                        You've been selected for a chance to get one year of
-                        subscription to use Wikipedia for free!
-                      </p>
-                    </div>
-                  </div>
+                  <Taproom />
                 </div>
 
                 <div className="divider lg:divider-horizontal"></div>
 
-                <div className="grid flex-grow h-72 w-72   card bg-base-300 rounded-box place-items-center shadow-2xl">
+                <div className="grid flex-grow h-72 w-72 card bg-base-300 rounded-box place-items-center shadow-2xl">
                   <figure>
                     <img src={quiz} alt="Quiz" height={120} width={120} />
                   </figure>
@@ -234,23 +172,7 @@ const Projects = () => {
                     id="my-modal-12"
                     className="modal-toggle"
                   />
-                  <div className="modal modal-bottom sm:modal-middle">
-                    <div className="modal-box relative">
-                      <label
-                        htmlFor="my-modal-12"
-                        className="btn btn-sm btn-circle absolute right-2 top-2"
-                      >
-                        ✕
-                      </label>
-                      <h3 className="text-lg font-bold text-accent">
-                        Congratulations random Interner user!
-                      </h3>
-                      <p className="py-4">
-                        You've been selected for a chance to get one year of
-                        subscription to use Wikipedia for free!
-                      </p>
-                    </div>
-                  </div>
+                  <LanguageQuiz />
                 </div>
               </div>
             </div>
